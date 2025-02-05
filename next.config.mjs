709c154ddd98242ec4c50881274be90bad923e00
Fiza Awan <awanfiza95@gmail.com>
2025-02-05 19:13:54 +0500

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,13 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
+        protocol: "https",
+        hostname: "cdn.sanity.io",
       },
     ],
+  },
+  experimental: {
+    esmExternals: false, // Fixes potential ESM issues in .mjs
   },
 };
 
